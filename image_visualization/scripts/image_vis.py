2c7588a16d1e5b7054cfcf13da9af5d3ce3274c7
kushtimusPrime <kush_hari@berkeley.edu>
2023-08-17 22:54:38 -0700

    Added CV2 imshow when you subscribe to compressed image

--- image_visualization/scripts/image_vis.py
+++ image_visualization/scripts/image_vis.py
@@ -2,14 +2,16 @@
 
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 import cv2
+import numpy as np
 
 class ImageVisualization(Node):
     def __init__(self):
         super().__init__('image_visualization')
-        self.subscription = self.create_subscription(Image, '/new_raw', self.image_callback, 10)
+        #self.subscription = self.create_subscription(Image, '/new_raw', self.image_callback, 10)
+        self.subscription = self.create_subscription(CompressedImage,'/image_raw/compressed',self.compressed_image_callback,10)
         self.subscription  # prevent unused variable warning
         self.bridge = CvBridge()
 
@@ -21,7 +23,13 @@ class ImageVisualization(Node):
             return
 
         cv2.imshow("Received Image", cv_image)
-        cv2.waitKey(1)  # This is necessary for the OpenCV window to update
+        cv2.waitKey(2)  # This is necessary for the OpenCV window to update
+
+    def compressed_image_callback(self,msg):
+        image_np = self.bridge.compressed_imgmsg_to_cv2(msg,'bgr8')
+        print(image_np.shape)
+        cv2.imshow("Received image",image_np)
+        cv2.waitKey(2)
 
 def main(args=None):
     rclpy.init(args=args)
